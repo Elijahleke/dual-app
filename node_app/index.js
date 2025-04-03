b123e78b29298ed9f741c3cfe7901cbc780229aa
elijahleke <adeleked@gmail.com>
2025-04-02 23:41:28 -0400

    123

--- node_app/index.js
+++ node_app/index.js
@@ -3,10 +3,10 @@ const { Pool } = require('pg');
 const app = express();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  host: 'localhost',
+  user: 'devops',
+  password: 'admin123',
+  database: 'sharedappdb'
 });
 
 app.get('/', async (req, res) => {
