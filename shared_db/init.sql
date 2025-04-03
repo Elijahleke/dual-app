5aa7aa0bab7bbe633d1b8d8a4cf307ca06259167
elijahleke <adeleked@gmail.com>
2025-04-02 23:14:58 -0400

    on conflict

--- shared_db/init.sql
+++ shared_db/init.sql
@@ -1,12 +1,16 @@
 CREATE DATABASE sharedappdb;
 \c sharedappdb;
 
+-- Drop the devs table if it exists to clear old data
+DROP TABLE IF EXISTS devs;
+
 CREATE TABLE devs (
   id SERIAL PRIMARY KEY,
   name VARCHAR(50)
 );
 
-INSERT INTO devs (name) VALUES ('Elijah Adeleke'), ('Debby Boyo'), ('Precious');
+INSERT INTO devs (name) VALUES ('Elijah Adeleke'), ('Debby Boyo'), ('Precious')
+ON CONFLICT (name) DO NOTHING;
 
 DELETE FROM devs
 WHERE name IN ('Flask Developer', 'Node Developer', 'Shared DB User');
